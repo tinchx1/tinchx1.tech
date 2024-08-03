cd0cb89e6bcf4d4eec0bededc884d9f26f87b966
tinchx1 <tinchomarchi34@gmail.com>
2024-08-02 22:26:24 -0300

    chore: Actualizar imagen personal en la página de inicio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
+import astroI18next from "astro-i18next";
 
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
+  integrations: [tailwind(), astroI18next(), robotsTxt()],
   site: 'https://tinchx1.tech/'
 })
